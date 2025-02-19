0f23ae9f43e39be69aed57a5361994103aab976d
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-19 01:12:49 -0800

    faq section finish

--- routes/web.php
+++ routes/web.php
@@ -203,15 +203,16 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
         Route::delete('/destroy/{id}', [ContentCommentController::class, 'destroy'])->name('admin.content.comment.destroy');
 });
 
-      //faq
-      Route::prefix('faq')->group(function(){
-        Route::get('/', [FAQController::class, 'index'])->name('admin.content.faq.index');
-        Route::get('/create', [FAQController::class, 'create'])->name('admin.content.faq.create');
-        Route::post('/store', [FAQController::class, 'store'])->name('admin.content.faq.store');
-        Route::get('/edit/{id}', [FAQController::class, 'edit'])->name('admin.content.faq.edit');
-        Route::put('/update/{id}', [FAQController::class, 'update'])->name('admin.content.faq.update');
-        Route::delete('/destroy/{id}', [FAQController::class, 'destroy'])->name('admin.content.faq.destroy');
-});
+         //faq
+         Route::prefix('faq')->group(function () {
+            Route::get('/', [FAQController::class, 'index'])->name('admin.content.faq.index');
+            Route::get('/create', [FAQController::class, 'create'])->name('admin.content.faq.create');
+            Route::post('/store', [FAQController::class, 'store'])->name('admin.content.faq.store');
+            Route::get('/edit/{faq}', [FAQController::class, 'edit'])->name('admin.content.faq.edit');
+            Route::put('/update/{faq}', [FAQController::class, 'update'])->name('admin.content.faq.update');
+            Route::delete('/destroy/{faq}', [FAQController::class, 'destroy'])->name('admin.content.faq.destroy');
+            Route::get('/status/{faq}', [FAQController::class, 'status'])->name('admin.content.faq.status');
+        });
   //menu
       Route::prefix('menu')->group(function(){
         Route::get('/', [MenuController::class, 'index'])->name('admin.content.menu.index');
