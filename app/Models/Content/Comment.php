b6f8067667918cc9686d0d62429d0dd9cd00591c
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-19 00:41:00 -0800

    finish posts section add model , request and controller files

---
+++ app/Models/Content/Comment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Content;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Comment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['body', 'parent_id', 'author_id', 'commentable_id', 'commentable_type', 'approved', 'status'];
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+
+
+}
