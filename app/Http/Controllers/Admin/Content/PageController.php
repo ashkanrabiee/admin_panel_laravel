e33f2641f0f5bae316d97c2f25064f770f7bd29c
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-19 02:19:28 -0800

    create page section

--- app/Http/Controllers/Admin/Content/PageController.php
+++ app/Http/Controllers/Admin/Content/PageController.php
@@ -1,22 +1,29 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Content;
+namespace App\Http\Controllers\admin\content;
 
-use App\Http\Controllers\Controller;
+use App\Models\Content\Page;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\PageRequest;
 
 class PageController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('admin.content.page.index');
+        $pages = Page::orderBy('created_at', 'desc')->simplePaginate(15);
+        return view('admin.content.page.index', compact('pages'));
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -25,41 +32,84 @@ class PageController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PageRequest $request)
     {
-        //
+        $inputs = $request->all();
+        $page = Page::create($inputs);
+        return redirect()->route('admin.content.page.index')->with('swal-success', 'صفحه  جدید شما با موفقیت ثبت شد');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(string $id)
+    public function show($id)
     {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit(string $id)
+    public function edit(Page $page)
     {
-        //
+        return view('admin.content.page.edit', compact('page'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id)
+    public function update(PageRequest $request, Page $page)
     {
-        //
+        $inputs = $request->all();
+        // $inputs['slug'] = null;
+        $page->update($inputs);
+        return redirect()->route('admin.content.page.index')->with('swal-success', 'صفحه  شما با موفقیت ویرایش شد');
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
+    public function destroy(Page $page)
     {
-        //
+        $result = $page->delete();
+        return redirect()->route('admin.content.page.index')->with('swal-success', 'صفحه  شما با موفقیت حذف شد');
     }
+
+
+    public function status(Page $page){
+
+        $page->status = $page->status == 0 ? 1 : 0;
+        $result = $page->save();
+        if($result){
+                if($page->status == 0){
+                    return response()->json(['status' => true, 'checked' => false]);
+                }
+                else{
+                    return response()->json(['status' => true, 'checked' => true]);
+                }
+        }
+        else{
+            return response()->json(['status' => false]);
+        }
+
+    }
+
+
 }
