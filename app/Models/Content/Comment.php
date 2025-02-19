dbd123b809731db9559a840a2d9e501a0edf92a3
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-19 02:44:58 -0800

    content comments section finish

--- app/Models/Content/Comment.php
+++ app/Models/Content/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Content;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -15,6 +16,22 @@ class Comment extends Model
     public function commentable()
     {
         return $this->morphTo();
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id');
+    }
+
+    public function answers()
+    {
+        return $this->hasMany($this, 'parent_id');
     }
 
 
