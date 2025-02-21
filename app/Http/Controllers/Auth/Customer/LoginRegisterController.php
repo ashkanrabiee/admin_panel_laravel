947bc8ee80b8e428915f9ecbb2a0df096fc817f5
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-21 02:43:12 -0800

    create login register form use otp registration for this project

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -2,16 +2,70 @@
 
 namespace App\Http\Controllers\Auth\Customer;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Customer\loginReqisterRequest;
+use App\Models\User;
 
 class LoginRegisterController extends Controller
 {
-    public function loginRegisterForm()
-    {
+    public function loginRegisterForm(){
         return view('customer.auth.login-register');
     }
 
-    
+    public function loginRegister(loginReqisterRequest $request){
+      
+        $inputs = $request->all();
 
+        //check id is email or not
+
+        if(filter_var($inputs['id'] , FILTER_VALIDATE_EMAIL)){
+
+            $type = 1 ; // 1  => email 
+            $user = User::where('email' , $inputs['id'])->first();
+            if(empty($user)){
+                $newUser['email'] = $inputs['id'];
+
+            }
+        }
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$' , $inputs['id'])){
+            $type = 0 ; // 0 => mobile
+
+            // all mobile numbers are in on format 9** *** ***
+            $inputs['id'] = ltrim($inputs['id'], '0');
+            $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'], 2) : $inputs['id'];
+            $inputs['id'] = str_replace('+98', '', $inputs['id']);
+
+            $user = User::where('mobile' , $inputs['id'])->first();
+            if(empty($user)){
+                $newUser['mobile'] = $inputs['id'];
+
+            }
+        }
+        
+        else{
+            $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
+            return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
+        }
+
+        if(empty($user))
+        {
+            $newUser['password'] = '98355154';
+            $newUser['activation'] = '1';
+            $user = User::create($newUser);
+
+        }
+
+        //create otp code
+
+        
+
+
+
+
+
+
+
+
+    }
 }
--- resources/views/customer/auth/login-register.blade.php
+++ resources/views/customer/auth/login-register.blade.php
@@ -4,7 +4,7 @@
 
 
 <section class="vh-100 d-flex justify-content-center align-items-center pb-5">
-    <form action="" method="post">
+    <form action="{{route('auth.customer.login-register')}}" method="post">
         @csrf
     <section class="login-wrapper mb-5">
         <section class="login-logo">
@@ -13,19 +13,20 @@
         <section class="login-title">ورود / ثبت نام</section>
         <section class="login-info">شماره موبایل یا پست الکترونیک خود را وارد کنید</section>
         <section class="login-input-text">
-            <input type="text" name="id" value="{{ old('id') }}">
-            @error('id')
-            <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                <strong>
-                    {{ $message }}
-                </strong>
-            </span>
-        @enderror
+            <input type="text" name="id" value="{{old('id')}}">
         </section>
+        @error('id')
+        <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+            <strong>
+                {{ $message }}
+            </strong>
+        </span>
+    @enderror
+
         <section class="login-btn d-grid g-2"><button class="btn btn-danger">ورود به آمازون</button></section>
         <section class="login-terms-and-conditions"><a href="#">شرایط و قوانین</a> را خوانده ام و پذیرفته ام</section>
     </section>
-    </form>
+</form>
 </section>
 
 
