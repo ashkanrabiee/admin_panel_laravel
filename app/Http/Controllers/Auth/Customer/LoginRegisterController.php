61eeb16327d1164a20cf89a0a7015c4f8c4c5b95
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-02-22 03:29:49 -0800

    send otp message successful

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -7,74 +7,100 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Config;
+// use App\Http\Services\Message\MessageSerivce;
+use App\Http\Services\Message\MessageSerivce;
+use App\Http\Services\Message\SMS\SmsService;
+use App\Http\Requests\Auth\Customer\LoginRegisterRequest;
 use App\Http\Requests\Auth\Customer\loginReqisterRequest;
+use App\Http\Services\Message\MessageService;
 
 class LoginRegisterController extends Controller
 {
-    public function loginRegisterForm(){
+    public function loginRegisterForm()
+    {
         return view('customer.auth.login-register');
     }
 
-    public function loginRegister(loginReqisterRequest $request){
-      
+    public function loginRegister(loginReqisterRequest $request)
+    {
         $inputs = $request->all();
 
         //check id is email or not
-
-        if(filter_var($inputs['id'] , FILTER_VALIDATE_EMAIL)){
-
-            $type = 1 ; // 1  => email 
-            $user = User::where('email' , $inputs['id'])->first();
+        if(filter_var($inputs['id'], FILTER_VALIDATE_EMAIL))
+        {
+            $type = 1; // 1 => email
+            $user = User::where('email', $inputs['id'])->first();
             if(empty($user)){
                 $newUser['email'] = $inputs['id'];
-
             }
         }
-        elseif(preg_match('/^(\+98|98|0)9\d{9}$' , $inputs['id'])){
-            $type = 0 ; // 0 => mobile
+
+        //check id is mobile or not
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$/', $inputs['id'])){
+            $type = 0; // 0 => mobile;
+
 
             // all mobile numbers are in on format 9** *** ***
             $inputs['id'] = ltrim($inputs['id'], '0');
             $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'], 2) : $inputs['id'];
             $inputs['id'] = str_replace('+98', '', $inputs['id']);
 
-            $user = User::where('mobile' , $inputs['id'])->first();
+            $user = User::where('mobile', $inputs['id'])->first();
             if(empty($user)){
                 $newUser['mobile'] = $inputs['id'];
-
             }
         }
-        
+
         else{
             $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
             return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
         }
 
-        if(empty($user))
-        {
+        if(empty($user)){
             $newUser['password'] = '98355154';
-            $newUser['activation'] = '1';
+            $newUser['activation'] = 1;
             $user = User::create($newUser);
-
         }
 
         //create otp code
-
-        $otpCode = rand(111111,999999);
+        $otpCode = rand(111111, 999999);
         $token = Str::random(60);
-        $otpInput = [
-                'token' =>  $token ,
-                'user_id' => $user->id ,
-                'opt_code' => $otpCode,
-                'login_id' => $inputs['id'],
-                'type' => $type,
+        $otpInputs = [
+            'token' => $token,
+            'user_id' => $user->id,
+            'otp_code' => $otpCode,
+            'login_id' => $inputs['id'],
+            'type' => $type,
         ];
 
-        Otp::create($otpInput);
+        Otp::create($otpInputs);
 
+        //send sms or email
 
+        if($type == 0){
+            //send sms
+            $smsService = new SmsService();
+            $smsService->setFrom(Config::get('sms.otp_from'));
+            $smsService->setTo(['0' . $user->mobile]);
+            $smsService->setText("مجموعه آمازون \n  کد تایید : $otpCode");
+            $smsService->setIsFlash(true);
 
+            $messagesService = new MessageService($smsService);
 
+        }
+
+        $messagesService->send();
+
+        
+        // dd('hi');
 
     }
+
+
+
+
+
+
+
 }
